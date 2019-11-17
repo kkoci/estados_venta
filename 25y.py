5207dc5565b4ab531edf9d9f49db826a66161f6d
Kristian <kristian@slave-node.home>
2019-11-17 02:02:49 +0000

    [FIX] Removed .py scripts

--- 25y.py
+++
@@ -1,8 +0,0 @@
-import operator
-import csv
-
-filename = "MobilePhoneMasts.csv"
-def load_source(filename):
-    with open(filename, "r") as f:
-        reader = csv.reader(f, delimiter=";")
-        return filter(lambda x: x[12] in ("25 years"), list(reader))
